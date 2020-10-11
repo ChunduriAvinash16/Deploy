37f61fd086dd6d198c6219fde8126eb1cd797535
Avinash <chunduriaviansh6@gmail.com>
2020-10-12 00:41:47 +0530

    Deploy

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Header</h1>
+      </header>
+      <div className="container">
+        <div className="row">
+          <h3>Portfolio</h3>
+        </div>
+      </div>
+      <footer className="App-footer">
+        <h2>Footer</h2>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
