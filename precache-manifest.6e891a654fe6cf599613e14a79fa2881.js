9185326cfb528d10e409c86ca90e7ba6afc672d2
Avinash <chunduriaviansh6@gmail.com>
2020-10-12 00:43:06 +0530

    Updates

---
+++ precache-manifest.6e891a654fe6cf599613e14a79fa2881.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e3e9e5bce0aaec53ce7c4680fab4a9",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "cacf63b9a666a8ad084e",
+    "url": "/deploy/static/css/main.ecd4c7fd.chunk.css"
+  },
+  {
+    "revision": "4ce4e1dc695616cb343d",
+    "url": "/deploy/static/js/2.77c7c82d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy/static/js/2.77c7c82d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cacf63b9a666a8ad084e",
+    "url": "/deploy/static/js/main.2392c93b.chunk.js"
+  },
+  {
+    "revision": "c8b35b66d67afcc69f53",
+    "url": "/deploy/static/js/runtime-main.6de565d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
