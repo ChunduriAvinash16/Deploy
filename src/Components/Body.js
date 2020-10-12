4bd7972aa10deeff84295704db2c2d673758fcfa
Avinash <chunduriaviansh6@gmail.com>
2020-10-12 14:51:49 +0530

    12

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Body() {
     return (
         <div>
-            Portfolio
+            Good Morning
         </div>
     )
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
 function Footer() {
     return (
-        <div className="footer">
-            Footer
+        <div className="footer" id="Footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        Hello
+                    </div>
+                    <div className="col-md-6" id="Media">
+                        <a href="https://www.linkedin.com/in/avinash-chunduri-58a1bb1a0/" id="link"><LinkedInIcon/></a>
+                        <a href="https://www.facebook.com/aviansh.chunduri/" id="link"><FacebookIcon/></a>
+                        <a href="#" id="link"><InstagramIcon/></a>
+                        <a href="https://github.com/ChunduriAvinash16" id="link"><GitHubIcon/></a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
